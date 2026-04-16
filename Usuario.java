80c11b8ae8e2a80ad6b5199ce5c2770d6da54eff
rogerrdeveloper <rogeriocavalcanti455@gmail.com>
2026-04-16 15:04:43 -0300

    Update Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -4,8 +4,20 @@ public class Usuario {
     public Usuario(String nome) {
         this.nome = nome;
     }
+    
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
     public String getNome() {
         return nome;
+    }
+
+    public int getIdade() {
+        return idade;
     }
 }
